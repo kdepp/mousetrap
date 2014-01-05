42f01d5405c33364ad43811d825243bd58f86d0d
KDepp <kdepp.cd@gmail.com>
2014-01-05 16:39:05 +0800

    add: xlxs_5sing.js, daily.sh updated

--- daily.sh
+++ daily.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+/usr/local/bin/node /Users/kdepp/projects/git/mousetrap/app/xiami.js 
+/usr/local/bin/node /Users/kdepp/projects/git/mousetrap/app/weibo.js 
+/usr/local/bin/node /Users/kdepp/projects/git/mousetrap/app/douban.js 
+/usr/local/bin/node /Users/kdepp/projects/git/mousetrap/app/5sing.js 
 /usr/local/bin/node /Users/kdepp/projects/git/mousetrap/app/xlsx_xiami.js 
 /usr/local/bin/node /Users/kdepp/projects/git/mousetrap/app/xlsx_weibo.js 
 /usr/local/bin/node /Users/kdepp/projects/git/mousetrap/app/xlsx_douban.js 
+/usr/local/bin/node /Users/kdepp/projects/git/mousetrap/app/xlsx_5sing.js 
+/usr/local/bin/node /Users/kdepp/projects/git/mousetrap/mail.js 
