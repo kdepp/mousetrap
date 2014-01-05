ca085c3e876a853d13d2932699ee012a47244fe7
KDepp <kdepp.cd@gmail.com>
2014-01-05 13:42:14 +0800

    add: daily shell to export xlsx

---
+++ daily.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/local/bin/node /Users/kdepp/projects/git/mousetrap/app/xlsx_xiami.js 
+/usr/local/bin/node /Users/kdepp/projects/git/mousetrap/app/xlsx_weibo.js 
+/usr/local/bin/node /Users/kdepp/projects/git/mousetrap/app/xlsx_douban.js 
