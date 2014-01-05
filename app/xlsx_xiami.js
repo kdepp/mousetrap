a6eff906d9b5d3899dbd5056ecab1bc348b1df5c
KDepp <kdepp.cd@gmail.com>
2014-01-05 13:41:49 +0800

    add: douban, weibo, xiami export to xlsx

---
+++ app/xlsx_xiami.js
@@ -0,0 +1,11 @@
+var exportXlsx = require("../mysql_to_xlsx"),
+	path = require("path");
+
+exportXlsx.mysql_to_xlsx({
+	host     : '127.0.0.1',
+	user     : 'root',
+	password : '123123lu',
+	database : 'mousetrap',
+	table	 : 'xiami_artist_site',
+	dir		 : path.join(__dirname, "..")
+});
